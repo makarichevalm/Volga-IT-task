4700ffc570330f0488f6e8e373bc6981b6d8dc4d
makarichevalm <makarichevalm@yandex.ru>
2022-05-31 22:52:57 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './css/App.css';
 import MainPage from './pages/MainPage/MainPage';
 import FirstPage from './pages/FirstPage/FirstPage';
@@ -20,20 +20,34 @@ import EighthPage from './pages/EightPage/EightPage';
 import NinthPage from './pages/NinePage/NinePage';
 import TenthPage from './pages/TenPage/TenPage';
 import FinPage from './pages/FinallyPage/FinallyPage';
-import Ten from './pages/TenPage/TenPage';
 function App({ url }) {
     console.log(url);
     return (
         <div className="main">
             <BrowserRouter>
-                <Routes></Routes>
+                <Routes>
+                    <Route path="/" exact element={<MainPage />} />
+                    <Route path="/first" exact element={<FirstPage />} />
+                    <Route path="/secondm" exact element={<SecondPageM />} />
+                    <Route path="/secondw" exact element={<SecondPageW />} />
+                    <Route path="/betweenone" exact element={<Between23 />} />
+                    <Route path="/thirdone" exact element={<ThirdPage1 />} />
+                    <Route path="/thirdtwo" exact element={<ThirdPage2 />} />
+                    <Route path="/fourthone" exact element={<FourthPage1 />} />
+                    <Route path="/fourthtwo" exact element={<FourthPage2 />} />
+                    <Route path="/betweentwo" exact element={<Between4 />} />
+                    <Route path="/fiftheye" exact element={<FifthPage1 />} />
+                    <Route path="/fifthsun" exact element={<FifthPage2 />} />
+                    <Route path="/sixmen" exact element={<SixthPageM />} />
+                    <Route path="/sixwomen" exact element={<SixthPageW />} />
+                    <Route path="/sixboth" exact element={<SixthPageB />} />
+                    <Route path="/seventh" exact element={<SeventhPage />} />
+                    <Route path="/eighth" exact element={<EighthPage />} />
+                    <Route path="/ninth" exact element={<NinthPage />} />
+                    <Route path="/tenth" exact element={<TenthPage />} />
+                    <Route path="/finally" exact element={<FinPage />} />
+                </Routes>
             </BrowserRouter>
-            <Ten />
-            {/* <Third1 /> */}
-            {/* <Between /> */}
-            {/* <SecondPageW /> */}
-            {/* <MainPage /> */}
-            {/* <FirstPage /> */}
         </div>
     );
 }
